dafc7df1e450d0a7cc59d0823b630e2e76ce90e1
Mangal Singh <mangalsingh2e4n@gmail.com>
2025-12-16 15:10:01 +0530

    america

--- 1-basic.py
+++ 1-basic.py
@@ -5,4 +5,4 @@ age=23
 DOB= 15-7-2003
 print("my name is :",Name)
 print("My age is:", age)
-
+print(type(DOB))
---
+++ 4-Datatypes.py
@@ -0,0 +1,18 @@
+#INTEGER> it contains number such as +,-,0
+a=5
+#STRING> it contains group of letters
+b= "mangal singh"
+#FLOAT> it contains numbers with decimal values in it
+c=8.9
+#BOOLEAN> it contains or store values such as TRUE or FALSE
+d= True
+e= False 
+#NONE> it just says none
+f= None
+#printinf there types
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
