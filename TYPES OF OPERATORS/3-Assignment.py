c18df0207c70b742446cb1f8a59102204e789f9a
Mangal Singh <mangalsingh2e4n@gmail.com>
2025-12-16 16:18:51 +0530

    america

---
+++ TYPES OF OPERATORS/3-Assignment.py
@@ -0,0 +1,26 @@
+# = >> equal to operator, =+ >> assigns a new adding value to previous one
+# =- >> substracts the value which is assigned to it, =* >> multiplies with the value assigned to it
+# /= >> divides with the assignes value to it , **= >> power multiplication
+
+a= 35
+print(a)
+
+b=32
+b += 3
+print(b) 
+
+c=21
+c *= 3
+print(c)
+
+d=33
+d /= 11
+print(d)
+
+e= 20
+e -= 3
+print(e)
+
+f=2
+f **= 3
+print(f)
