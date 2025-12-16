8a8fa5b6a0a5d3c84e7e568de3fe4705ac146f06
Mangal Singh <mangalsingh2e4n@gmail.com>
2025-12-16 16:43:35 +0530

    america

--- TAKING INPUTS/1-Inputs.py
+++ TAKING INPUTS/1-Inputs.py
@@ -1,3 +1,9 @@
 #Taking inputs from users
 name= input("enter your name:-")
 print("Hii,and welocme to python learning", name)
+
+#ddition of two numbers
+a= int(input("please enter the value:"))
+b= int(input("please enter the value:"))
+c=a+b
+print("sum of two given input integer is:", c)
