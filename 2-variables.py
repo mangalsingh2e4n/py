15752f26c630c35a96b915d650d11635250d65ca
Mangal Singh <mangalsingh2e4n@gmail.com>
2025-12-16 15:42:59 +0530

    america

--- 2-variables.py
+++
@@ -1,6 +0,0 @@
-Name="Mangal Singh"     # ( = ) is considered assignment 
-age=23
-DOB= 15-7-2003
-print("my name is :",Name)
-print("My age is:", age)
-print(type(DOB))
---
+++ TYPES OF OPERATORS/2-relational.py
@@ -0,0 +1,14 @@
+#(==) >>> means equal to
+#(!=) >>> means not equal
+#(<=) >>> means less than or equal 
+#(>=) >>> means greater than or equal 
+#(>) >>> greater than
+#(<) >>> less than
+a=43
+b=32
+print(a == b)  # relational operators gives output in form of boolean values such as True or False
+print(a != b)
+print(a >= b)
+print(a <= b)
+print(a<b)
+print(a>b)
