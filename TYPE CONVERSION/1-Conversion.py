0eb9f70f909527fd6e802aa2993929fc76fff57e
Mangal Singh <mangalsingh2e4n@gmail.com>
2025-12-16 16:34:46 +0530

    america

---
+++ TYPE CONVERSION/1-Conversion.py
@@ -0,0 +1,15 @@
+# converts one type of data to another automatically
+# Example
+a=4
+b=3
+sum= a+b
+print(sum)
+#==BEFORE  CONVERSION==
+e="4"
+f=5
+print(e+f)
+# if anyone practicing using this code note that before conversion failes then next part will not execute even it is true so please comment it before moving to next part
+#==AFTER CONVERSION==
+c= int("4")
+d= 5
+print(c+d)
