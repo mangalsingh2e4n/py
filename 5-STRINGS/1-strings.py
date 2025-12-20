0a1a5ab60c44b56ca11bfc7041588240fecea6e2
Mangal Singh <mangalsingh2e4n@gmail.com>
2025-12-20 16:12:08 +0530

    america

---
+++ 5-STRINGS/1-strings.py
@@ -0,0 +1,8 @@
+intro="mera naam hai BULLA,rakhta hoon khulla"
+print(intro)
+# if we have to print the next next sentence in new line we use ESCAPE SEQUENCE CHARACTER like \n--> for new line and \t-->for space tab
+#EXAMPLE
+selfintro="mera naam hai BULLA.\nRakhta hoon khulla."
+print(selfintro)  #\n-->for new line
+selfintr="mera naam hai BULLA\tRakhta hoon khulla."
+print(selfintr)
---
+++ 5-STRINGS/2-concatenation.py
@@ -0,0 +1,10 @@
+#concatenation means adding/merging of two or more strings in a single line
+name= "MAHENDRA DAMORDAS MODI LOVE "
+nature="KATTAPA/AMIT SHAH"
+print(name+nature)
+
+# we can store added string in other variable
+name1="JAB TAK RAHEGA SAMOSE ME AALU,"
+name2="TAB TAK RAHEGA BIHAR ME LALU"
+BIHAR= name1+name2
+print(BIHAR)
