55b7a968d707df9ac04bd8bd941f24061e013603
Mangal Singh <mangalsingh2e4n@gmail.com>
2025-12-21 22:46:09 +0530

    america

---
+++ 6-CONDITIONSAL STATEMENTS/1-conditions.py
@@ -0,0 +1,30 @@
+#THERE ARE 3 CONDITIONAL STATEMENTS they are (if,elif and else)
+
+#1-(if)-->it only prints if the statement is true
+#EXAPMLE:
+
+# age=18
+# age-16
+# if(age>=18):
+#     print("He/She has right to have marriage")
+
+# #elif-->it  only works when the if condition fails
+# age=16
+# if(age>=18):
+#     print("He/She has right to have marriage")
+
+# elif(age<18):
+#     print("He/She has no right to have marriage")
+
+#else--> it only works if bot if and elif conditions fails
+
+light="yellow"
+if(light== "green"):
+    print("chal naa chutiye")
+elif(light=="red"):
+    print("Rukja laude")
+elif(light=="yellow"):
+    print("Thookne ke liye taiyaar")
+else:
+    print("are iski maaka")
+
