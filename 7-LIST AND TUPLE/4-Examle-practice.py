7cbfe2e07509e066dfae8947f52f2fbb4ad9a30e
Mangal Singh <mangalsingh2e4n@gmail.com>
2025-12-26 20:15:16 +0530

    PYTHON

--- 7-LIST AND TUPLE/4-Examle-practice.py
+++ 7-LIST AND TUPLE/4-Examle-practice.py
@@ -1,43 +1,43 @@
 # # #program to enter the 3 mob=vie names and store them in the list
 
-# # movie_1=input("enter the name of the movie-1=")
-# # movie_2=input("enter the name of the movie-2=")
-# # movie_3=input("enter the name of the movie-3=")
-# # movie=[movie_1,movie_2,movie_3]
+movie_1=input("enter the name of the movie-1=")
+movie_2=input("enter the name of the movie-2=")
+movie_3=input("enter the name of the movie-3=")
+movie=[movie_1,movie_2,movie_3]
 
-# # print(movie)
-
-
-# # #2-code to check the entered word is aplindrome or not
-# # word = input("Enter the word = ")
-
-# # if word == word[::-1]: #--> it reverses the entered word
-# #     print("The word is palindrome")
-# # else:
-# #     print("The word is not palindrome")
+print(movie)
 
 
-# #3-code to check the entered value is aplindrome or not in list
-# num = [1, 1, 1, 1, 1, 1]
-# # Original list
+# #2-code to check the entered word is aplindrome or not
+word = input("Enter the word = ")
 
-# num_1 = num.copy()
-# # Creates a copy of the original list so the original is not changed
+if word == word[::-1]: #--> it reverses the entered word
+    print("The word is palindrome")
+else:
+    print("The word is not palindrome")
 
-# num_1.reverse()
-# # Reverses the copied list in place
 
-# if num == num_1:
-# # Compares original list with reversed list
+#3-code to check the entered value is aplindrome or not in list
+num = [1, 1, 1, 1, 1, 1]
+# Original list
 
-#     print("The entered list is palindrome")
-#     # Executes if both lists are same
+num_1 = num.copy()
+# Creates a copy of the original list so the original is not changed
 
-# else:
-#     print("The entered list is not palindrome")
-#     # Executes if both lists are different
+num_1.reverse()
+# Reverses the copied list in place
 
-#count the number of students with grade A
+if num == num_1:
+# Compares original list with reversed list
+
+    print("The entered list is palindrome")
+    # Executes if both lists are same
+
+else:
+    print("The entered list is not palindrome")
+    # Executes if both lists are different
+
+# count the number of students with grade A
 
 students_grades=("A","B","D","E","F","C","A","E","C","D","A","A","F")
 print("The number of students with GRADE-A are=",students_grades.count("A"))
