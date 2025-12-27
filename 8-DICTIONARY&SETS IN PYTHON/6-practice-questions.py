7977fea3b10d52e3a2dea34d4b49956f62548217
Mangal Singh <mangalsingh2e4n@gmail.com>
2025-12-27 22:51:57 +0530

    PYTHON

--- 8-DICTIONARY&SETS IN PYTHON/6-practice-questions.py
+++ 8-DICTIONARY&SETS IN PYTHON/6-practice-questions.py
@@ -6,7 +6,7 @@ dictionary={
 }
 print(dictionary)
 
-#EXAMPLE-2--->You are given a list of subjects for students. Assume one classroom is required for 1 subject. How many classrooms are needed by all students.-->"python", "java", "C++", "python", "javascript" "java", "python", "java", "C++", "C".
+#EXAMPLE-2--->You are given a list of subjects for students. Assume one classroom is required for 1 subject. How many classrooms are needed by all students.-->"python", "java", "C++", "python", "javascript" "java", "python", "java", "C++", "C"
 subject={
     "python", "java", "C++", "python", "javascript" "java", "python", "java", "C++", "C"
 }
